ba09432af114129f38828a920969e44bc6ec51d9
0zyl02 <1147894228@qq.com>
2019-09-02 17:54:05 +0800

    'lesson-1'

---
+++ src/components/Count.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="count">
+    <hr />
+    <p>Count.vue:{{todos}}</p>
+    <h1>count.vue:{{count}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Count",
+  components: {},
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    todos() {
+      return this.$store.state.todos;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
